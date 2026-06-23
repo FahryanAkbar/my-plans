93ddfc8f7cf2632d5d1ad0975d65a959fa654084
Fahryan Akbar <fahryanakbar05@gmail.com>
2026-06-23 17:19:51 +0700

    hotfix: edit domain convex

--- frontend/convex/auth.config.ts
+++ frontend/convex/auth.config.ts
@@ -1,5 +1,7 @@
 import { AuthConfig } from "convex/server";
 
+declare const process: { env: Record<string, string | undefined> };
+
 export default {
   providers: [
     {
@@ -9,7 +11,7 @@ export default {
       // See https://docs.convex.dev/auth/clerk#configuring-dev-and-prod-instances
       // domain: 'https://national-swine-87.clerk.accounts.dev',
       // domain: "https://clerk.myplans.my.id",
-      domain: "https://national-swine-87.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN || "https://national-swine-87.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
